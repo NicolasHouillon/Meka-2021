9d32f41522317acfa7e6f8321c14ea7f1602df16
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2021-04-06 16:44:52 +0200

    add images

--- server/data/pg.sql
+++ server/data/pg.sql
@@ -17,7 +17,7 @@ create table question (
     quizz_id int references quizz(id)
 );
 
-insert into quizz(name, image, keywords) values ('Culture informatique', 'informatique.jpg', '{"informatique", "culture"}'), ('Star Wars', 'star-wars.jpg', '{"culture", "films"}');
+insert into quizz(name, image, keywords) values ('Culture informatique', 'informatique.jpg', '{"informatique", "culture"}'), ('Star Wars', 'star-wars.png', '{"culture", "films"}');
 insert into question(enonce, points, anwsers, is_image, quizz_id) values
     (
      'Par qui à été créé GitHub ?',
