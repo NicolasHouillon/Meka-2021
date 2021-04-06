f97ae395a9512782ec9ed28aaf5f07bdaa2d96e4
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2021-04-06 14:09:07 +0200

    possibilité d'afficher les images

--- server/server.js
+++ server/server.js
@@ -5,4 +5,5 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(router); // Requests processing will be defined in the file router
+app.use('/img', express.static('img'));
 app.listen(port, () => console.log('Server app listening on port ' + port));
