d7dfd703994c2185447aee6db10ee38c3f5a23d7
karl remy <karl.remy@hotmail.com>
2021-04-06 12:22:06 +0200

    ajout navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import {Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Quizz</Nav.Link>
+          <Nav.Link href="#pricing">Compte</Nav.Link>
+        </Nav>
+      </Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
