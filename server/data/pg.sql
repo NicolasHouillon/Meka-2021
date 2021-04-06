552c57a53dfe7bfe9c0bfb1de68414ff439ca83a
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2021-04-06 10:50:01 +0200

    init bd

---
+++ server/data/pg.sql
@@ -0,0 +1,17 @@
+drop schema if exists quizz;
+create schema quizz;
+set search_path to quizz;
+
+create table quizz (
+    id serial primary key,
+    name varchar,
+    keywords array
+)
+
+create table question (
+    id serial primary key,
+    enonce varchar,
+    points int,
+    anwsers array,
+    quizz_id references quizz(id)
+)
