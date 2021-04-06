35369816b557acc49466dca453f20c021c0a5524
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2021-04-06 11:25:59 +0200

    fix pg.sql

--- server/data/pg.sql
+++ server/data/pg.sql
@@ -5,13 +5,14 @@ set search_path to quizz;
 create table quizz (
     id serial primary key,
     name varchar,
-    keywords array
-)
+    keywords varchar[]
+);
 
 create table question (
     id serial primary key,
     enonce varchar,
     points int,
-    anwsers array,
-    quizz_id references quizz(id)
-)
+    anwsers varchar[][2],
+    quizz_id int references quizz(id)
+);
+
