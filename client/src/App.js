03ad91c14d50b203190658dd5f4b6bc1991342eb
etienne <etiennedoutrelon@gmail.com>
2021-04-06 17:38:27 +0200

    ajout authentification dans la navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Quizz from './components/Quizz';
 import Signin from "./components/authentification/signin";
 import {Signup} from "./components/authentification/signup";
+import {Link, Router} from "@reach/router";
 
 function App() {
 
   return (
     <div className="App">
         <Navbar bg="light" variant="light">
-            <Navbar.Brand href="#home">Logo</Navbar.Brand>
+            <Navbar.Brand href="/">Logo</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="#features">Quizz</Nav.Link>
-                <Nav.Link href="#pricing">Compte</Nav.Link>
+                <Link to="/quizz">Quizz</Link>
+                <Link to="/connexion">Se connecter</Link>
+                <Link to="/enregistrement">S'enregistrer</Link>
             </Nav>
         </Navbar>
         <header className="App-header">
-            <Quizz/>
+            <Router>
+                <Quizz path="/quizz"/>
+                <Signup path="/connexion"/>
+                <Signin path="/enregistrement"/>
+            </Router>
+            <h1>Bienvenu sur l'application de quizz !</h1>
         </header>
     </div>
   );
