13748b1b1d6340e181112ca5f803292d1f683a50
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2021-04-06 13:36:02 +0200

    update BD

--- server/data/pg.sql
+++ server/data/pg.sql
@@ -12,40 +12,53 @@ create table question (
     enonce varchar,
     points int,
     anwsers varchar[][2],
+    is_image boolean,
     quizz_id int references quizz(id)
 );
 
 insert into quizz(name, keywords) values ('Culture informatique', '{"informatique", "culture"}'), ('Star Wars', '{"culture", "films"}');
-insert into question(enonce, points, anwsers, quizz_id) values
+insert into question(enonce, points, anwsers, is_image, quizz_id) values
     (
      'Par qui à été créé GitHub ?',
      3,
      '{{"Ronald Wayne", "faux"}, {"Linus Torvalds", "vrai"}, {"Sid SijBrandij", "faux"}, {"Paul Allen", "faux"}}',
+     FALSE,
      1
     ),
     (
      'En Markdown, parmi les propositions suivantes, quelles balises permettent de formater du texte ?',
      2,
      '{{"˜˜X˜˜", "vrai"}, {"_X_", "vrai"}, {"||X||", "vrai"}, {"|X|", "faux"}, {"```X```", "vrai"}}',
+     FALSE,
      1
     ),
     (
      'Quel est le protocole (langage) d`échange de fichier entre un client et un serveur sur internet ?',
      2,
      '{{"SMTP", "faux"}, {"AUX", "faux"}, {"FTP", "vrai"}, {"TCP/IP", "faux"}}',
+     FALSE,
      1
     );
 
-insert into question(enonce, points, anwsers, quizz_id) values
+insert into question(enonce, points, anwsers, is_image, quizz_id) values
     (
      'En quelle année à été réalisé Star Wars: La menace fantôme ?',
      2,
      '{{"1999", "vrai"}, {"2000", "faux"}, {"2001", "faux"}}',
+     FALSE,
      2
     ),
     (
      'Combien de films Star Wars y a t-il en tout ? (Star Wars et A Star Wars Story)',
      4,
      '{{"12", "vrai"}, {"9", "faux"}, {"11", "faux"}}',
+     FALSE,
+     2
+    ),
+    (
+     'Laquelle de ces peluches est un Ewok ?',
+     2,
+     '{{"wookiee.jpg", "faux"}, {"ewok.jpg", "vrai"}, {"Porg.jpg", "faux"}}',
+     TRUE,
      2
     );
