afe3ca688f391e974a6c389bcb292d12d492e06a
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2021-04-06 17:03:31 +0200

    front v1 list-quizz

--- client/src/components/Quizz.js
+++ client/src/components/Quizz.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Card from "react-bootstrap/Card";
 
 export default function Quizz() {
     const [quizz, setQuizz] = useState([]);
@@ -21,14 +22,22 @@ export default function Quizz() {
 
     return (
         <>
-            <ol>
-                {quizz.map((quizz, index) =>
-                    <li key={index}>
-                        {quizz.name} ---
-                        {quizz.keywords}
-                    </li>
+            <div className="row" style={{display: "flex"}}>
+                {quizz.map((quizz) =>
+                    <div className="col-lg-6">
+                        <Card style={{width: 452}} className="m-lg-5">
+                            <img src={"http://localhost:8000/img/" + quizz.image} height="300px" width="450px"/>
+                            <h3 className="text-dark">{quizz.name}</h3>
+
+                            <div className="col-12 text-right mb-2">
+                                <button className="btn btn-dark col-lg-4">
+                                    Faire le quizz
+                                </button>
+                            </div>
+                        </Card>
+                    </div>
                 )}
-            </ol>
+            </div>
         </>
     )
 }
