35fc7cc234e670a8f0f7846b5784bdbf0abb68b6
Nicolas Houillon <nicolas_houillon@ens.univ-artois.fr>
2021-04-08 20:08:06 +0200

    fix tests

--- client/cypress/integration/quizz/quizz.spec.js
+++ client/cypress/integration/quizz/quizz.spec.js
@@ -1,58 +1,58 @@
 describe('Quizz',  ()  =>  {
     it('content',  ()  =>  {
         let url = cy.visit('/quizz/1');
-        url.get('h2').contains('Culture informatique');
-        url.get('h5').contains('Par qui à été créé GitHub ?');
+        url.get('img');
+        url.get('div').should('have.class', 'quizz-anwser').contains('Par qui à été créé GitHub ?');
 
         url.get('.anwser').its('length').should('eq', 4);
-        url.get('.anwser').should('have.class', 'btn-secondary');
+        url.get('.anwser').should('have.class', 'btn-answer');
 
-        url.get('.btn-info').its('length').should('eq', 1);
-        url.get('.btn-info').contains('Valider');
+        url.get('.btn-valider').its('length').should('eq', 1);
+        url.get('.btn-valider').contains('Valider');
     });
 
     it('response ', function () {
         let url = cy.visit('/quizz/1');
         let btn = url.get('.anwser').contains('Ronald Wayne');
 
-        btn.click().should('have.class', 'btn-primary');
-        btn.click().should('have.class', 'btn-secondary');
+        btn.click().should('have.class', 'btn-answer-select');
+        btn.click().should('have.class', 'btn-answer');
     });
 
     it('Valider', function () {
         let url = cy.visit('/quizz/1');
-        let h2 = url.get('h2').invoke('text');
-        let h5 = url.get('h5').invoke('text');
+        let h5 = url.get('div').should('have.class', 'quizz-anwser').invoke('text');
 
-        url.get('.btn-info').click();
+        url.get('.btn-valider').click();
 
-        url.get('h2').invoke('text').should('not.eq', h2);
-        url.get('h5').invoke('text').should('not.eq', h5);
+        url.get('div').should('have.class', 'quizz-anwser').invoke('text').should('not.eq', h5);
     });
 
     it('results', function () {
         let url = cy.visit('/quizz/1');
+        let h5 = url.get('div').should('have.class', 'quizz-anwser').invoke('text');
 
-        url.get('.valider').click();
-        url.get('.valider').click();
-        url.get('.valider').click();
+        url.get('.btn-valider').click();
+        url.get('.btn-valider').click();
+        url.get('.btn-valider').click();
 
         url.get('.anwser').its('length').should('eq', 12);
-        url.get('h5').its('length').should('eq', 3);
+        url.get('p').should('have.class', 'quizz-anwser').its('length').should('at.least', 3);
 
-        url.get('p').should('have.class', 'font-weight-bold').contains('Votre score : 0')
-    })
+        url.get('p').should('have.class', 'score').contains('Votre score : 0');
+    });
 
     it('retour', function () {
         let url = cy.visit('/quizz/1');
+        let h5 = url.get('div').should('have.class', 'quizz-anwser').invoke('text');
 
-        url.get('.valider').click();
-        url.get('.valider').click();
-        url.get('.valider').click();
+        url.get('.btn-valider').click();
+        url.get('.btn-valider').click();
+        url.get('.btn-valider').click();
 
-        url.get('.btn-info').contains('Retour aux quizz');
-        url.get('.btn-info').click();
+        url.get('.btn-valider').contains('Retour aux quizz');
+        url.get('.btn-valider').click();
 
         cy.url().should('eq', 'http://localhost:3000/quizz');
-    })
+    });
 });
